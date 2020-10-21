d2ab72f23a2e980162c33579b2ddd3092fca376f
Andrew <andrew.casin@gmail.com>
2020-10-20 23:47:35 -0500

    commit tree 42f40996878a40b219f7d6c698942f270047fd25

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,5 @@
 # Build your say_hello method here
+# Build a method that can be called with an optional argument by defining that method with a default argument.
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
+end
